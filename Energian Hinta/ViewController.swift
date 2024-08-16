d59a0b545d1f82767610ded1326d4e651d19adab
Veikko Arvonen <arvonenveikko@gmail.com>
2024-08-16 22:23:37 +0300

    Initial Commit

---
+++ Energian Hinta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Energian Hinta
+//
+//  Created by Veikko Arvonen on 16.8.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
