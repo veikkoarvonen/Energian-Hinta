aaa7aae8ddb5c14bb95099ebb4183771df801c83
Veikko Arvonen <arvonenveikko@gmail.com>
2024-08-23 16:42:48 +0300

    Make daily prises work

--- Energian Hinta/Model/Converter.swift
+++ Energian Hinta/Model/Converter.swift
@@ -10,6 +10,9 @@ import Foundation
 protocol PriceSetDelegate: AnyObject {
     func setPrice(price: Double?)
     func updateSheet(price: Double?)
+    func updatePriceArray(prices: [HourlyPrice])
+    func passFetchedPrice(price: HoursPrice)
+    func sortFetchedPrices()
 }
 
 struct PriceManager {
@@ -54,8 +57,121 @@ struct PriceManager {
         }
     }
     
+    
+    func fetchLatestPrices() {
+        let URL = "https://api.porssisahko.net/v1/latest-prices.json"
+        performDayRequest(with: URL)
+    }
+    
+    func performDayRequest(with url: String) {
+        if let url = URL(string: url) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print("Error in day URL session")
+                    return
+                }
+                
+                
+                
+                if let safeData = data {
+                    parseLatestJSON(data: safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseLatestJSON(data: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(ElectricityPriceResponse.self, from: data)
+        
+            delegate?.updatePriceArray(prices: decodedData.prices)
+        } catch {
+            print("Could not parse latest JSON")
+        }
+    }
+    
 }
 
 struct ElectricityPrice: Codable {
     var price: Double
+}
+
+struct ElectricityPriceResponse: Codable {
+    let prices: [HourlyPrice]
+}
+
+struct HourlyPrice: Codable {
+    let price: Double
+    let startDate: String
+    let endDate: String
+}
+
+struct HoursPrice: Codable {
+    var hour: String
+    var price: Double?
+}
+
+struct PriceManager2 {
+    let hourStrings = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
+    let formatter = DateFormatter()
+    var JSONresults: [HoursPrice] = []
+    var delegate: PriceSetDelegate?
+    
+    func fetchDailyPrices(for date: Date) {
+        let dispatchGroup = DispatchGroup()
+        
+        
+        for i in 0..<hourStrings.count {
+            dispatchGroup.enter()
+            performRequest(for: date, hour: hourStrings[i])
+            dispatchGroup.leave()
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            delegate?.sortFetchedPrices()
+        }
+    }
+    
+    func performRequest(for date: Date, hour: String) {
+        formatter.dateFormat = "YYYY-MM-dd"
+        let dateString = formatter.string(from: date)
+        let hourString = hour
+        let URLstring = "https://api.porssisahko.net/v1/price.json?date=\(dateString)&hour=\(hourString)"
+        if let url = URL(string: URLstring) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print("Error in URL session")
+                    let failedPrice = HoursPrice(hour: hour, price: nil)
+                    delegate?.passFetchedPrice(price: failedPrice)
+                    return
+                }
+                if let safeData = data {
+                    parseLatestJSON(data: safeData, hour: hour)
+                } else {
+                    let failedPrice = HoursPrice(hour: hour, price: nil)
+                    delegate?.passFetchedPrice(price: failedPrice)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseLatestJSON(data: Data, hour: String) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(ElectricityPrice.self, from: data)
+            let price = decodedData.price
+            let fetchedPrice = HoursPrice(hour: hour, price: price)
+            delegate?.passFetchedPrice(price: fetchedPrice)
+        } catch {
+            let failedPrice = HoursPrice(hour: hour, price: nil)
+            delegate?.passFetchedPrice(price: failedPrice)
+        }
+    }
+    
+    
 }
--- Energian Hinta/ViewController.swift
+++ Energian Hinta/ViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController, PriceSetDelegate {
+    
+    
     var sheetLabels = [UILabel]()
     var sheetLines = [UIView]()
-    let labelYPositions = [450.0, 400.0, 350.0, 300.0, 250.0, 200.0]
-    let labelTexts = ["25", "20", "15", "10", "5", "0"]
+    var pricesToDisplay = [HourlyPrice]()
+    var priceViews = [UIView]()
     
+    let labelYPositions = [450.0, 400.0, 350.0, 300.0, 250.0, 200.0]
+    let labelTexts = ["","","","","",""]
+    
+    var dailyPrices: [HoursPrice] = []
     
     @IBOutlet weak var testLabel: UILabel!
     
@@ -21,7 +27,11 @@ class ViewController: UIViewController, PriceSetDelegate {
         setPriceSheet()
         var priceManager = PriceManager()
         priceManager.delegate = self
-        priceManager.fetchPrice(from: Date())
+        var priceManager2 = PriceManager2()
+        priceManager2.delegate = self
+        priceManager2.fetchDailyPrices(for: Date())
+        //priceManager.fetchPrice(from: Date())
+        //priceManager.fetchLatestPrices()
 
     }
 
@@ -53,10 +63,44 @@ class ViewController: UIViewController, PriceSetDelegate {
             sheetLines.append(line)
             
         }
+        
+        let sheetWidth = view.frame.width - 40
+        let hourWidth = sheetWidth / 24
+        
+        for i in 0..<24 {
+            let label = UILabel()
+            label.text = "\(i)"
+            label.textColor = .systemGray
+            label.textAlignment = .center
+            label.font = UIFont(name: "Optima", size: 10.0)
+            view.addSubview(label)
+            let x = 35 + (hourWidth / 2) + (hourWidth * Double(i))
+            label.frame = CGRect(x: 10, y: 470, width: hourWidth, height: 30)
+            label.center.x = x
+        }
+        
     }
     
     
+    func passFetchedPrice(price: HoursPrice) {
+        DispatchQueue.main.async {
+            self.dailyPrices.append(price)
+        }
+    }
     
+    func sortFetchedPrices() {
+        DispatchQueue.main.async {
+            self.dailyPrices.sort { $0.hour < $1.hour }
+            var prices: [Double?] {
+                var p: [Double?] = []
+                for price in self.dailyPrices {
+                    p.append(price.price)
+                }
+                return p
+            }
+            self.displayDailyPrices(prices: prices)
+        }
+    }
     
     
     
@@ -109,6 +153,104 @@ class ViewController: UIViewController, PriceSetDelegate {
             }
         }
     }
+    
+    func updatePriceArray(prices: [HourlyPrice]) {
+        DispatchQueue.main.async {
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "YYYY-MM-dd"
+            
+            let todaysString = formatter.string(from: Date())
+            
+            for i in 0..<prices.count {
+                if prices[i].startDate.contains(todaysString) {
+                    self.pricesToDisplay.append(prices[i])
+                }
+            }
+            self.getDailyPrices()
+        }
+    }
+    
+    func getDailyPrices() {
+        var prices: [Double?] = []
+        
+        let priceFilters = ["00:00:00", "01:00:00", "02:00:00", "03:00:00", "04:00:00", "05:00:00", "06:00:00", "07:00:00", "08:00:00", "09:00:00", "10:00:00", "11:00:00", "12:00:00", "13:00:00", "14:00:00", "15:00:00", "16:00:00", "17:00:00", "18:00:00", "19:00:00", "20:00:00", "21:00:00", "22:00:00", "23:00:00"]
+
+        for i in 0..<priceFilters.count {
+            
+            let filter = priceFilters[i]
+            if let p = pricesToDisplay.first(where:  { $0.startDate.contains(filter) }) {
+                prices.append(p.price)
+            } else {
+                prices.append(nil)
+            }
+        }
+        print(pricesToDisplay)
+        displayDailyPrices(prices: prices)
+        
+    }
+    
+    func displayDailyPrices(prices: [Double?]) {
+        
+        if let highestPrice = prices.compactMap({ $0 }).max() {
+            
+            var maxPrice: Double = 0
+            while maxPrice < highestPrice {
+                maxPrice += 5
+            }
+            
+            for i in 0..<sheetLabels.count {
+                let priceForLabel = maxPrice / 5 * Double(i)
+                sheetLabels[i].text = "\(Int(priceForLabel))"
+            }
+            
+            let screenWidth = view.frame.width
+            let sheetWidth = screenWidth - 40
+            let hourWidth = sheetWidth / 24
+            
+            let viewWidth = hourWidth - 5
+            
+            for i in 0..<prices.count {
+                let priceView = UIView()
+                priceView.backgroundColor = UIColor(named: "theme")
+                view.addSubview(priceView)
+                
+                let viewWidth = hourWidth - 5
+                let centerX = 35 + (hourWidth / 2) + (hourWidth * Double(i))
+                
+                var y: Double {
+                    if let p = prices[i] {
+                        let h = 250 * (p / maxPrice)
+                        return 465 - h
+                    } else {
+                        return 465
+                    }
+                }
+                
+                var height: Double {
+                    if let p = prices[i] {
+                        let h = 250 * (p / maxPrice)
+                        return h
+                    } else {
+                        return 1
+                    }
+                }
+                
+                priceView.frame = CGRect(x: 30, y: y, width: viewWidth, height: height)
+                priceView.center.x = centerX
+                
+                
+            }
+            
+            
+            
+            
+        } else {
+            print("The array contains only nil values or is empty")
+        }
+        
+    }
+    
     
     
 }
